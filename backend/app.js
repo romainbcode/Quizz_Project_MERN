b52a3ea9567ffc4631f23dca3bfc6843bf007ad9
romain bizot <romain-bizot@hotmail.fr>
2023-07-29 16:15:19 +0200

    add code for userModel and errorResponse

---
+++ backend/app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+require('dotenv').config()
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+var cors = require('cors')
+var cookieParser = require('cookie-parser');
+
+//configuration of socket.io
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const errorHandler = require('./middleware/error');
+
+//import routes
+const authRoutes = require('./routes/authRoutes');
+
+//database connexion
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser:true,
+    useUnifiedTopology : true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(()=>{console.log("DB connected")})
+.catch((err)=>{console.log(err)})
+
+//middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+//ROUTES MIDDLEWARE
+app.use('/api', authRoutes);
+
+//error middleware
+app.use(errorHandler);
+
+//port 
+const port = process.env.PORT || 9000
+
+io.on('connection', (socket)=>{
+    console.log('a user is connected', socket.id);
+})
+
+exports.io = io;
+
+server.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
---
+++ backend/models/userModel.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        trim: true,
+        required:[true, 'Please add a username'],
+        unique: true,
+        minlength: [5, 'Username need minimum of 5 caracters'],
+        maxlength: [15, 'Username need maximum of 15 caracters']
+    },
+    email:{
+        type: String,
+        trim: true,
+        require: [true, "Please add an email"],
+        unique: true,
+        match:[
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "Please add a valid email"
+        ]
+    },
+    password:{
+        type: String,
+        trim: true,
+        required: [true, "Please add a Password"],
+        minlength : [6, "Password must have more than 6 characters"],
+        match:[
+            /^(?=.*\d)(?=.*[@#\-_$%^&+=§!\?])(?=.*[a-z])(?=.*[A-Z])[0-9A-Za-z@#\-_$%^&+=§!\?]+$/,
+           'Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number and a special characters'
+        ]
+    },
+    role: {
+        type:String,
+        default: 'user'
+    }
+}, {timestamps: true})
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        next()
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+})
+
+userSchema.methods.comparePassword = async function(yourPassword){
+    return await bcrypt.compare(yourPassword, this.password)
+}
+
+//Token for staying connected during 3 hours
+userSchema.methods.getJwtToken = async function(){
+    return jwt.sign({id: this.id}),
+    process.env.JWT_SECRET,{
+        expires: 3600*3
+    }
+}
+
+module.exports = mongoose.model('User', userSchema)
