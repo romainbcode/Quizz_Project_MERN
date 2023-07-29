66465a033998527d47be59ed21a992272fb38f6f
romain bizot <romain-bizot@hotmail.fr>
2023-07-29 17:11:55 +0200

    authController : signin with generation of token and route signin

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -24,3 +24,44 @@ exports.signup = async(req, res, next)=>{
         next(error)
     }
 }
+
+const sendTokenResponse = async(user, codeStatus, res)=>{
+    const token = await user.getJwtToken();
+    res
+    .status(codeStatus)
+    .cookie('token', token, {maxAge: 3*60*60*1000, httpOnly: true})
+    .json({
+        success: true,
+        id: user._id,
+        role: user.role
+    })
+
+}
+exports.signin = async(req, res, next)=>{
+    try{
+        const {email, password} = req.body;
+
+        if(!email){
+            return next(new ErrorResponse('Please add an email', 403));
+        }
+        if(!password){
+            return next(new ErrorResponse('Please add a password', 403));
+        }
+
+        const user = await User.findOne({email});
+        if(!user){
+            return next(new ErrorResponse("This email is not used", 400))
+        }
+
+        const isMatched = await user.comparePassword(password)
+        if(!isMatched){
+            return next(new ErrorResponse("Password wrong", 400));
+        }
+
+        //sendTokenResponse(user, 200, res);
+
+
+    }catch(error){
+        next(error)
+    }
+}
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const { signup } = require('../controllers/authController')
+const { signup, signin } = require('../controllers/authController')
 const router = express.Router()
 
 //Auth routes
 
 //SignUp route
 router.post('/signup', signup);
+//SignIn route
+router.post('/signin', signin)
 
 module.exports = router;
