683744c48d25a24ad68c1a1b6fff427ec18b7297
romain bizot <romain-bizot@hotmail.fr>
2023-08-03 20:07:18 +0200

    add transition on Card

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -28,15 +28,21 @@ export default function PostCard ({
     }) {
 
   return (
-    <Card sx={{ maxWidth: 345, p : 2, color: 'primary.themewhite', bgcolor: 'primary.mainGreenDark', borderRadius: '10%', boxShadow: '0 3px 10px #000' }}>
+    <Card sx={{ maxWidth: 350, p : 2, color: 'primary.themewhite', bgcolor: 'primary.mainGreenDark', 
+    borderRadius: '10%', boxShadow: '0 3px 10px #000', padding: '20px, 20px', height: 300
+    }}>
+        
+        <CardMedia
+            component="img"
+            sx={{ height: '85%', borderRadius: '20px',transitionDuration: '1s', "&:hover": { cursor: "pointer", height: "55%", transitionDuration: '1s' }}}
+         
+
+        
+            image={image}
+        />
         <Typography gutterBottom variant="h5" component="div">
             {title}
             </Typography>
-        <CardMedia
-            component="img"
-            sx={{ height: 200 }}
-            image={image}
-        />
         <CardContent>
             <Typography variant="body2">
             {subheader}
