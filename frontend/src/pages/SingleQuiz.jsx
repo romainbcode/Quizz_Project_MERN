43db158a44eb0c325f2980b55e9127dbc8043f7e
romain bizot <romain-bizot@hotmail.fr>
2023-08-14 20:45:49 +0200

    refactoring and change send button

--- frontend/src/pages/SingleQuiz.jsx
+++ frontend/src/pages/SingleQuiz.jsx
@@ -1,58 +1,33 @@
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import CommentIcon from '@mui/icons-material/Comment';
-import ShareIcon from '@mui/icons-material/Share';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Box, Button, Divider, Grid } from '@mui/material';
+import { Card, CardContent, Typography, Box, Button } from '@mui/material';
 import axios from 'axios';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
-import moment from 'moment';
-import { useSelector } from 'react-redux';
-import TextareaAutosize from '@mui/base/TextareaAutosize';
 import { toast } from 'react-toastify';
 import AnswerList from '../components/AnswerList';
 import Loader from '../components/Loader'
+import SendIcon from '@mui/icons-material/Send'
 
 const SinglePost = () => {
     
-
-    const { userInfo } = useSelector(state => state.signIn);
-
     const [title, setTitle] = useState('');
-    const [usernameCreater, setUsernameCreater] = useState('');
     const [subheader, setSubheader] = useState('');
     const [image, setImage] = useState('');
     const [createdAt, setCreatedAt] = useState('');
-    const [answer, setAnswer] = useState([]);
     const [questionAnswer, setQuestionAnswer] = useState([])
     const [loading, setLoading] = useState(false);
     const [totalGoodAnswer, setTotalGoodAnswer] = useState(0);
 
     const { id } = useParams();
 
-    const [num, setNum] = useState(0);
     const [answerClicked, setAnswerClicked] = useState(0);
 
     const [loadLocalStorage, setLoadLocalStorage] = useState(false)
 
     const updateNumberGoodAnswer=()=>{
         setAnswerClicked(answerClicked+1)
-        console.log("k,o")
-        console.log(JSON.parse(localStorage.getItem('test2')))
     }
 
-    //fetch single post
     const displaySinglePost = async () => {
         setLoading(true);
         try {
@@ -78,40 +53,32 @@ const SinglePost = () => {
              });
             if (data.success === true) {
                 toast.success("score quiz added");
-                //displaySinglePost();
             }
         } catch (error) {
             console.log(error);
             toast.error(error);
         }
     }
+
     useEffect(() => {
-        console.log("reset")
-        localStorage.setItem('test2', JSON.stringify(0))
+        localStorage.setItem('answerTrueFalse', JSON.stringify(0))
     }, [loadLocalStorage])
 
     useEffect(() => {
         displaySinglePost();
-        
     }, [])
 
     return (
         <>
-            <Navbar />
+            <Navbar titlePage={`${title}`}/>
             <Box sx={{display: 'flex', justifyContent:'center', width: '100%', height:'100%', pt:2}}>
                 {
                 loading ? <Loader/> :
                     <Box sx={{display: 'flex', justifyContent:'center', width: '90%'}}>
                         <Card sx={{height: '100%', width: '100%', bgcolor:'transparent', boxShadow:'none'}}>
                             <Box sx={{color:'primary.themewhite', bgcolor: "primary.mainGreenDark", borderRadius: '20px', boxShadow: '0 3px 10px #000', m:2 }}>
-                                <CardHeader
-                                    title={title}
-                                />
-                                <Typography variant="body2" sx={{pb:2}}>
+                                <Typography variant="body2" sx={{p:2}}>
                                     <Box component='span' dangerouslySetInnerHTML={{ __html: subheader }}></Box>
-                                </Typography>
-                                <Typography variant="body2" sx={{}}>
-                                    {"Total of answer : " + totalGoodAnswer}
                                 </Typography>
                             </Box>
                             <CardContent sx={{mb:3, mt:3}}>
@@ -124,11 +91,14 @@ const SinglePost = () => {
                                     ))
                                 }
                             </CardContent>
-                            {"good answer : " + answerClicked}
-                            <Button onClick={()=>
-                            addScoreQuiz()
-                            }>
-                                VALIDER
+                            <Button 
+                                onClick={()=>
+                                    addScoreQuiz()
+                                }
+                                variant="contained" color="success"
+                                endIcon={<SendIcon />}
+                            > 
+                            Send
                             </Button>
                         </Card>
                     </Box>
