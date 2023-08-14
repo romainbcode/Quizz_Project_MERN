216feaebfc7495d8da515bda4c9b20f31c821029
romain bizot <romain-bizot@hotmail.fr>
2023-08-14 20:04:43 +0200

    refactoring and add title in navbar

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -1,62 +1,35 @@
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import CommentIcon from '@mui/icons-material/Comment';
-import ShareIcon from '@mui/icons-material/Share';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Box } from '@mui/material';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { useSelector } from 'react-redux';
-import { toast } from 'react-toastify';
-
-
-
 
 export default function PostCard ({
-    id,
-    title,
-    subheader,
-    image, 
+        id,
+        title,
+        subheader,
+        image, 
     }) {
 
   return (
     <Link to={`quiz/show/${id}`} style={{textDecoration: 'none'}}>
-
-    <Card sx={{ maxWidth: 350, p : 2, color: 'primary.themewhite', bgcolor: 'primary.mainGreenDark', 
-    borderRadius: '10%', boxShadow: '0 3px 10px #000', padding: '20px, 20px', height: 300
-    }}>
-        
-        <CardMedia
-            component="img"
-            sx={{ height: '85%', borderRadius: '20px',transitionDuration: '1s', "&:hover": { cursor: "pointer", height: "55%", transitionDuration: '1s' }}}
-            image={image}
-        />
-        <Typography gutterBottom variant="h5" component="div">
-            {title}
+        <Card sx={{ maxWidth: 350, p : 2, color: 'primary.themewhite', bgcolor: 'primary.mainGreenDark', 
+        borderRadius: '10%', boxShadow: '0 3px 10px #000', padding: '20px, 20px', height: 300
+        }}>
+            <CardMedia
+                component="img"
+                sx={{ height: '85%', borderRadius: '20px',transitionDuration: '1s', "&:hover": { cursor: "pointer", height: "55%", transitionDuration: '1s' }}}
+                image={image}
+            />
+            <Typography gutterBottom variant="h5" component="div">
+                {title}
             </Typography>
-        <CardContent>
-            <Typography variant="body2">
-            {subheader}
-            </Typography>
-        </CardContent>
-        
-        <CardActions disableSpacing>
-            <Box sx={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
-                <Box>
-                    {"likes"} 3 
-                </Box>       
-            </Box>
-        </CardActions>
-    </Card>
+            <CardContent>
+                <Typography variant="body2" sx={{color:'#CED4DA'}}>
+                    {subheader}
+                </Typography>
+            </CardContent>
+        </Card>
     </Link>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,64 +1,56 @@
 import React, { useEffect, useState } from 'react'
 import { Box, Container, Grid } from '@mui/material'
 import axios from 'axios';
-import moment from 'moment';
 import PostCard from '../components/PostCard';
 import Navbar from '../components/Navbar';
-import Typography from '@mui/material/Typography';
 import Loader from '../components/Loader'
 
 const Home = () => {
 
-  const [isloading, setLoading] = useState(false);
-  const [quizs, setQuizs] = useState([])
+    const [isloading, setLoading] = useState(false);
+    const [quizs, setQuizs] = useState([])
 
-  const showQuizs = async()=>{
-    setLoading(true);
-    try{
-      const { data } = await axios.get('/api/quizs/show')
-      setQuizs(data.quizs)
-      setLoading(false)
-    }catch(error){
-      console.log(error.response.data.error)
+    const showQuizs = async()=>{
+        setLoading(true);
+        try{
+            const { data } = await axios.get('/api/quizs/show')
+            setQuizs(data.quizs)
+            setLoading(false)
+        }catch(error){
+            console.log(error)
+        }
     }
-  }
 
-  useEffect(()=>{
-    showQuizs();
-    console.log(quizs)
-  }, []);
+    useEffect(()=>{
+        showQuizs();
+    }, []);
 
-
-  return (
+    return (
     <>
-      <Navbar/>
-      <Box sx={{minHeight: "100vh"}}>
-      <Typography variant='h5' sx={{ pt: 2, color: 'primary.themewhite', fontWeight: 'bold'}}> Home page  </Typography>
-          <Container sx={{pt:4, pb:5, height: "60vh", width: '100%'}}>
-            <Box sx={{flexGrow:1}}>
-              <Grid container spacing={{xs:2, md:3}} columns={{xs:4, sm:8, md:12}}>
-                
-                
-                {
-                    isloading ? <Loader/> : 
-                    quizs && quizs.map((quiz, index) =>(
-                        <Grid item xs={2} sm={4} md={4} key={index} >
-                        <PostCard
-                            id={quiz._id}
-                            title={quiz.title}
-                            subheader={quiz.subheader}
-                            image={quiz.image ? quiz.image.url : ''}
-                        />
-                        </Grid>
-                    ))
-                }
-                
-              </Grid>
-            </Box>
-          </Container>
-      </Box>
+        <Navbar titlePage={"Home page"}/>
+        <Box sx={{minHeight: "100vh"}}>
+            <Container sx={{pt:4, pb:5, height: "60vh", width: '100%'}}>
+                <Box sx={{flexGrow:1}}>
+                    <Grid container spacing={{xs:2, md:3}} columns={{xs:4, sm:8, md:12}}>   
+                    {
+                        isloading ? <Loader/> : 
+                        quizs && quizs.map((quiz, index) =>(
+                            <Grid item xs={2} sm={4} md={4} key={index} >
+                            <PostCard
+                                id={quiz._id}
+                                title={quiz.title}
+                                subheader={quiz.subheader}
+                                image={quiz.image ? quiz.image.url : ''}
+                            />
+                            </Grid>
+                        ))
+                    }
+                    </Grid>
+                </Box>
+            </Container>
+        </Box>
     </>
-  )
+    )
 }
 
 export default Home;
